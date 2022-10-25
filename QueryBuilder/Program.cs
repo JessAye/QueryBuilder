3b10b0b834bf7d6125dcb73ecdda7829521d2bd4
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2022-10-25 17:24:52 -0400

    inital commit

---
+++ QueryBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+    static void Main()
+    {
+        
+    }
+
+static void Connect()
+{
+    SqlConnection conn = new SqlConnection("server=localhost");
+}
+//create objects for each table
+    //use internal interface IClassModel{int ID{get; set;}}
