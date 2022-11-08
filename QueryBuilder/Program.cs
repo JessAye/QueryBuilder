1b316b0b47cb859ac6de55e246b4cf07f1e1c619
Jesse Arnold <ARNOLDJT@ETSU.EDU>
2022-11-07 23:55:44 -0500

    working read, start of create

--- QueryBuilder/Program.cs
+++ QueryBuilder/Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Reflection;
+using QueryBuilder;
+using Microsoft.Data.Sqlite;
 
-    static void Main()
+
+    string file = Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString()).ToString();
+    string path = Path.Combine(file, @"Data\", "Lab5.db");
+    Console.Write("PATH: "+ path);
+    SqliteConnection connection = new SqliteConnection("Data Source=" + @$"{path}");
+    if (File.Exists(path))
     {
+        Querybuilder.QueryBuilder test = new Querybuilder.QueryBuilder(path);
+        
+        //Create
+        
+        //Read
+        test.ReadAll<Author>(connection);
+        test.Dispose();
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
+    else
+    {
+        Console.WriteLine("File does not Exist");
+    }
+    
 
-static void Connect()
-{
-    SqlConnection conn = new SqlConnection("server=localhost");
-}
-//create objects for each table
-    //use internal interface IClassModel{int ID{get; set;}}
+    
